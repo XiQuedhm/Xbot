2e8dff5ab93ee3a61b8ae51e1baa90b387981d2f
XiQuedhm <xiquedhm233@outlook.com>
2023-05-22 23:03:53 +0800

    开发版本2(00002)

--- Server.py
+++ Server.py
@@ -6,49 +6,6 @@ import threading as thread
 import XbotLib as Lib
 import zipfile
 
-def loadPlugins():
-    for folder in ['./plugins/', './builtinPlugins/']:
-        for root, dirs, files in os.walk(folder):
-            for file in files:
-                if file.endswith('.zip'):
-                    zip_file = os.path.join(root, file)
-                    with zipfile.ZipFile(zip_file, 'r') as zip_ref:
-                        zip_ref.extractall(os.path.join('./cache/', os.path.splitext(file)[0]))
-    return os.path.abspath('./cache/')
-    #这个函数是ChatGPT写的，我也不太清楚咋运行的
-    #不过，它是用来加载插件的
-    
-#def loadPluginsFunctions():
-#    for root, dirs, files in os.walk('./cache/'):
-#        for file in files:
-#            if file == 'main.py':
-#                with open(os.path.join(root, file)) as f:
-#                    code = f.read()
-#                    exec(code)
-
-def runFunction(func, *args):
-    t = threading.Thread(target=func, args=args)
-    t.start()
-
-def loadPluginsFunctions():
-    data = {}
-    plugins = [0, {}]
-    commands = 0
-    for root, dirs, files in os.walk('./cache/'):
-        if root != './cache/':
-            for file in files:
-                if file.endswith('.py') and not file.endswith('.lib.py'):
-                    name = os.path.splitext(file)[0]
-                    file_path = os.path.join(root, file)
-                    folder_path = os.path.abspath(root)
-                    data[name] = [file_path, folder_path]
-                    commands += 1
-            folder_name = os.path.basename(root)
-            plugins[0] += 1
-            plugins[1][folder_name] = os.path.abspath(root)
-    result = {'data': data, 'plugins': plugins, 'commands': commands}
-    return result
-
 print('正在加载插件')
 loadPlugins()
 print('アトリは、高性能ですから!')
--- XbotLib.py
+++ XbotLib.py
@@ -9,8 +9,73 @@ fileWay = '/root/go-cqhttp/Xbot/'
 fileWay = '/storage/emulated/0/Quark/Download/Xbot/'
 #工作目录
 
+class Load :
+    #只有加载时会运行的函数归类到这里
+    def loadPlugins():
+    for folder in ['./plugins/', './builtinPlugins/']:
+        for root, dirs, files in os.walk(folder):
+            for file in files:
+                if file.endswith('.zip'):
+                    zip_file = os.path.join(root, file)
+                    with zipfile.ZipFile(zip_file, 'r') as zip_ref:
+                        zip_ref.extractall(os.path.join('./cache/', os.path.splitext(file)[0]))
+    return os.path.abspath('./cache/')
+    #这个函数是ChatGPT写的，我也不太清楚咋运行的
+    #不过，它是用来加载插件的
+    
+def runFunction(func, *args):
+    t = threading.Thread(target=func, args=args)
+    t.start()
+
+def loadPluginsFunctions():
+    data = {}
+    plugins = [0, {}]
+    commands = 0
+    for root, dirs, files in os.walk('./cache/'):
+        if root != './cache/':
+            for file in files:
+                if file.endswith('.py') and not file.endswith('.lib.py'):
+                    name = os.path.splitext(file)[0]
+                    file_path = os.path.join(root, file)
+                    folder_path = os.path.abspath(root)
+                    data[name] = [file_path, folder_path]
+                    commands += 1
+            folder_name = os.path.basename(root)
+            plugins[0] += 1
+            plugins[1][folder_name] = os.path.abspath(root)
+    result = {'data': data, 'plugins': plugins, 'commands': commands}
+    return result
+    
 class Internal :
     #和框架本身有关的类，其中定义运行时会调用的方法
+    def updateJson(filePath, key=None, value=None):
+    if value is None:
+        value = key
+        key = None
+    data = {}
+    if os.path.exists(filePath):
+        with open(filePath, 'r') as f:
+            try:
+                data = json.load(f)
+            except json.JSONDecodeError:
+                pass
+    if key:
+        keys = key.replace('[', '.').replace(']', '').split('.')
+        d = data
+        for k in keys[:-1]:
+            if k not in d:
+                d[k] = {}
+            d = d[k]
+        d[keys[-1]] = value
+    else:
+        if not isinstance(data, list):
+            data = []
+        data.append(value)
+    with open(filePath, 'w') as f:
+        json.dump(data, f)
+    return data
+    #GPT写的
+    #输入文件，键，值，直接更改
     def logInput(logType,logBody):
         #用来写运行日志的方法
         logType = str(logType)
